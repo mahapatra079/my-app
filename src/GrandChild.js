aaaacd86bb042bc5265416bcd5ad273e9043c508
mahapatra079 <mahapatra079@gmail.com>
2026-01-10 13:24:43 +0530

    Prop Drilling - Analysis

---
+++ src/GrandChild.js
@@ -0,0 +1,7 @@
+function GrandChild({ userName }) {
+  return (
+    <h2>Grand Child: {userName}</h2>
+  );
+}
+
+export default GrandChild;
---
+++ src/Parent.js
@@ -0,0 +1,12 @@
+import Chilld1 from "./Chilld1";
+
+function Parent({ userName }) {
+  return (
+    <div>
+      <h3>Parent Component - does NOT use the prop</h3>
+      <Chilld1 userName={userName} />
+    </div>
+  );
+}
+
+export default Parent;
