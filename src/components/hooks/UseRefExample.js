904d3cfc0a29760d59c7e0f8f7d7c6f52e38f617
mahapatra079 <mahapatra079@gmail.com>
2026-01-28 22:52:22 +0530

    Removed Unwanted Comments

--- src/components/hooks/UseRefExample.js
+++ src/components/hooks/UseRefExample.js
@@ -1,33 +1,4 @@
-// import { useRef } from "react";
 
-// function Reference() {
-
-//     const inputRef = useRef(null);
-//       const handleFocus = () => {
-//         console.log("Ref object:", inputRef);
-//         console.log("DOM element:", inputRef.current);
-//     inputRef.current.focus();
-//   };
-//     return (
-//     <div className="use-reference">
-//         <h4>Reference Component</h4>
-//         <div>
-//            <p> This is a simple reference component.</p>
-//             <input type="text" ref={inputRef} />
-//                 <button type="button" className="addBtn" onClick={handleFocus}>Focus input</button>
-//             </div>
-            
-                // useRef(null) → creates a ref
-                // ref={inputRef} → connects input to ref
-                // inputRef.current → actual DOM input
-                // .focus() → focuses input
-            
-            
-        // </div>
-    // );
-//  }
-
-// export default Reference; 
 
 import { useRef } from "react";
 
