d0cb2b1510fd905fe1ef6787f120d806f3736c16
mahapatra079 <mahapatra079@gmail.com>
2026-05-27 11:44:52 +0530

    Reducer changes

--- src/components/hooks/StateManagement/UseReducerExample.js
+++ src/components/hooks/StateManagement/UseReducerExample.js
@@ -1,28 +1,28 @@
 import {useReducer} from "react";
 
-function Counter() {  
+const initialState = {
+  count: 0
+}
 
-  const initialState = {
-    count: 0
+const reducer = (state, action) => {
+  console.log(state, action)
+  switch(action.type) {
+    case "INCREMENT":
+      return {count: state.count + 1 }
+    case "DECREMENT":
+      return { count: state.count - 1 }
+     // return state.count === 0 ? state : { count: state.count - 1 }; // guard in the DECREMENT case to prevent going below 0:
+    // return { count: Math.max(0, state.count - 1) }; //it returns the existing state unchanged — so the count stays at 0
+    //Math.max is slightly more readable.
+    
+    case "RESET":
+      return {count: 0}
+    default:
+      return state
   }
-  
-  const reducer = (state, action) => {
-    console.log(state, action)
-    switch(action.type) {
-      case "INCREMENT":
-        return {count: state.count + 1 }
-      case "DECREMENT":
-        return { count: state.count - 1 }
-       // return state.count === 0 ? state : { count: state.count - 1 }; // guard in the DECREMENT case to prevent going below 0:
-      // return { count: Math.max(0, state.count - 1) }; //it returns the existing state unchanged — so the count stays at 0
-      //Math.max is slightly more readable.
-      
-      case "RESET":
-        return {count: 0}
-      default:
-        return state
-    }
-   };
+};
+ 
+  function Counter() {  
 
   const [state, dispatch] = useReducer(reducer, initialState)
   
@@ -45,3 +45,7 @@ function Counter() {
 }
 
 export default Counter;
+
+// Performance
+//Inside → new function object allocated in memory on every render
+//Outside → created once, reused forever
