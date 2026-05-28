5b155b5c8aa123f89021cd8ebd0e175f9fa11e89
mahapatra079 <mahapatra079@gmail.com>
2026-05-28 15:17:42 +0530

    Destructuring

--- src/components/destructuring/Destructuring.jsx
+++ src/components/destructuring/Destructuring.jsx
@@ -5,7 +5,7 @@ export const Destructuring = () => {
         city: 'New York'
     };
     const { name, age, city } = person; // Destructuring 
-    console.log("Name:", name, "Age:", age, "City:", city); // Output: Name: John Age: 30 City: New York
+    console.log("Name:", name, "Age:", age, "City:", city ); // Output: Name: John Age: 30 City: New York
     return (
         <div className="card">
             <h2 className="font-bold text-lg mb-5">Destructuring -  Used to extract values from objects/arrays</h2>
@@ -16,18 +16,18 @@ export const Destructuring = () => {
     );
 }
 
+//Destructing Object
+const  user = 
+{
+    name:'Amit',
+    age:30
+};
 
-// let obj = {
-//     name: "Alice",
-//     age: 30,
-//     city: "New York"
-// };
+const {name,age} = user;
+console.log(name,age)
 
-// option1: let { name, age, city } = obj; - "destructuring"
-// option 2: let readname = obj.name;
-// option 3: let readage = obj["age"];
+// Destructurin  with arrays
+const color =['red','green','yellow'];
+const [first,second,_] = color  //first will be 'red', second will be 'green' and _ will be 'yellow' but we are not using it so we can ignore it by using _
 
-
-// Key point:
-    // Used everywhere (React + normal JS)
-    // Not related to lifecycle or state initialization
+console.log(first,second)
